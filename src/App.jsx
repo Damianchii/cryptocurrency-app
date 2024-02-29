8d3520978ee96ce3d8ace2bf19aa000b4113e562
Damianchii <damianczerwinsky@gmail.com>
2024-02-29 14:25:08 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 	const [coins, setCoins] = useState([])
 
 	const url =
-		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=30&page=1&sparkline=false&locale=en'
+		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=60&page=1&sparkline=false&locale=en'
 
 	useEffect(() => {
 		axios
